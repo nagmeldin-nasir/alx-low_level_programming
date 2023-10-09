61dc5382f2e5cb7e6e0a0a0195c0e9171921f771
nagmeldin <nagmeldin.nasir@gmail.com>
2023-10-09 10:24:10 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-	/**
-	 * main - Entry poin
-	 *
-	 * Description: A C program that prints with put function
-	 *
-	 * Return: Alawys 0 (Success)
-	 */  	 
-	int main(void)
-	{	
+/**
+ * main - Entry  point 
+ * Description : A C program that prints with put function 
+ * Returne: Always 0 (Success)
+*/
+int main(void)
+{	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0); 
-	}
+}
