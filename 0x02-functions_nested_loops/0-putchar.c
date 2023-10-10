f626f370853d934701e4626f97fc97181a22ebc5
nagmeldin <nagmeldin.nasir@gmail.com>
2023-10-10 15:34:20 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,14 @@
  * main - Entry point
  *
  * Description: prints _putchar using putchar prototype
- * 
+ *
  * Returne: Always 0 (Success)
 */
 int main(void)
 {
 char str[] = "_putchar";
 int ch;
-for(ch = 0;ch < 8; ch++)
+for (ch = 0; ch < 8; ch++)
 _putchar(str[ch]);
 _putchar('\n');
 return (0);
