1374757e9f369fc5f5baa4086b9ecee6808b789f
nagmeldin <nagmeldin.nasir@gmail.com>
2023-10-10 15:55:05 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphapet - utilizes on the _putchar fuction to print
+ *		the alphabet a - z
+*/
+int print_alphabet(void)
+{
+int ch;
+for (ch = 'a' ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
+
