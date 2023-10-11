a84f0018465485cbf19f823527dcba1dd1edd270
nagmeldin <nagmeldin.nasir@gmail.com>
2023-10-10 17:36:45 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@
 */
 int print_alphabet_x10(void)
 {
-	int line, ch;
-	for (line = 0 ;line <= 9; line++)
-		for (ch = 'a'; ch <= 'z'; ch++)
-    putchar(ch);
-    _putchar('\n');
+int line, ch;
+for (line = 0 ; line <= 9; line++)
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
