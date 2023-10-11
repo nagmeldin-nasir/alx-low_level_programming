e452b94142e528431061829c3d96c73c5765ad1d
nagmeldin <nagmeldin.nasir@gmail.com>
2023-10-11 11:30:48 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * Description: prints _putchar using putchar prototype
- *
- * print_alphabet_x10 - function that will print the alphabet 10 times
+ * print_alphabet_x10 - function that will print alphabet 10 times
  *
 */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 int line, ch;
 for (line = 0 ; line <= 9; line++)
