9b73b5d071854a2c8bbacc7b220b27edc77b3f82
nagmeldin <nagmeldin.nasir@gmail.com>
2023-10-12 15:20:10 -0700

    a

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,7 +6,7 @@
  * @b: second integer
  * @c: third integer
  * Return: largest number
- */
+*/
 
 int largest_number(int a, int b, int c)
 {
